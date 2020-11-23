c69e227771a0dc87268ad554b4bd038a0033626e
qhy555 <49610826+qhy555@users.noreply.github.com>
2020-11-23 15:44:51 +0800

    Create 112301.App.java
    qhy

---
+++ 112301.App.java
@@ -0,0 +1,18 @@
+package pac;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			int a = scanner.nextInt();
+			System.out.println("R"+"("+(int)(Math.random()*100)+","+(int)(Math.random()*100)+")");
+		}
+		
+	}   
+}
+	
+
+
