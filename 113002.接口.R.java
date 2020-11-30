2cd98f8072cdcde455cf91fd5681050c40e19c8e
qhy555 <49610826+qhy555@users.noreply.github.com>
2020-11-30 15:00:06 +0800

    Create 113002.接口.R.java

---
+++ 113002.接口.R.java
@@ -0,0 +1,7 @@
+package pac;
+
+public interface R {
+	static String boy_filePathString="img/boy.gif";
+	static String girl_filePathString="img/girl.gif";
+	static int[][]array2d={{1,2,3},{4,5,6}};
+}
