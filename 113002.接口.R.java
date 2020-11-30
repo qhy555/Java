6fb141e8281639147910c11f57484dd999685fd6
qhy555 <49610826+qhy555@users.noreply.github.com>
2020-11-30 15:28:18 +0800

    Update 113002.接口.R.java

--- 113002.接口.R.java
+++ 113002.接口.R.java
@@ -3,5 +3,9 @@ package pac;
 public interface R {
 	static String boy_filePathString="img/boy.gif";
 	static String girl_filePathString="img/girl.gif";
-	static int[][]array2d={{1,2,3},{4,5,6}};
+	static int[][]array2d={
+		{1,2},
+		{3,4},
+		{5,6}
+		};
 }
