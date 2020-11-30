c1cbf8eabece884efcd6aa00fc02578f6bdbb9fc
qhy555 <49610826+qhy555@users.noreply.github.com>
2020-11-30 14:58:49 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,10 @@
+package pac;
+
+public class App extends R {
+	public static void main(String[] args) {
+	System.out.println(R.boy_filePathString);
+	System.out.println(R.girl_filePathString);
+	System.out.println(R.array2d[0][1]);
+	}
+
+}
